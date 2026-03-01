cfcbb33d421edbde9d6b9343eed9e0e1bcca42c6
LumiDeveloper <nikiljaTSTU@gmail.com>
2026-03-01 22:47:01 +0300

    Django Shell Commit

--- yatube/Posts/views.py
+++ yatube/Posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
+from django.utils import timezone
+import datetime
 
 
 def index(request):
@@ -15,3 +17,16 @@ def group_posts(request, slug):
         'posts': posts,
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def morning_posts_view(request):
+    start_date = timezone.make_aware(datetime.datetime(1854, 7, 7))
+    end_date = timezone.make_aware(datetime.datetime(1854, 7, 21, 23, 59, 59))
+    
+    posts = Post.objects.filter(
+        text__icontains='утро',
+        author__username='leo',
+        pub_date__range=(start_date, end_date)
+    )
+    
+    return render(request, 'posts/index.html', {'posts': posts})
